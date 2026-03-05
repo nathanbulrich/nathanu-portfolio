a4d7a269177eb8ccba609b4e867971cc08e424e0
nathanbulrich <148499379+nathanbulrich@users.noreply.github.com>
2026-03-05 09:58:59 -0500

    hero update

--- src/app/components/HeroCard.tsx
+++ src/app/components/HeroCard.tsx
@@ -3,14 +3,14 @@ import DotRow from "./DotRow";
 export default function HeroCard() {
   return (
     <div className="px-6 md:px-12 lg:px-16 pt-8 pb-8 md:pt-[88px] md:pb-[56px]">
-      <div className="space-y-6 max-w-[624px]">
+      <div className="space-y-6 max-w-[664px]">
         <p className="leading-relaxed">
-          Hi, I&apos;m Nathan Ulrich. I&apos;m a designer in NYC who builds apps.
-          Currently I&apos;m a Discovery lead at Patreon.
+          Hi, I&apos;m Nathan Ulrich. I&apos;m a semi-technical designer in NYC who builds apps.
+          Currently I&apos;m working on Discovery and Community at Patreon.
         </p>
         <p className="leading-relaxed">
-          I believe great social software starts with focused
-          personal utility. Tools create the strongest networks.
+          I believe in the benefits of a social internet and want to help build it.
+          Great software starts with focused personal utility. Tools create quality networks.
         </p>
       </div>
 
